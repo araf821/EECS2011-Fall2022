1456178d25427c48d543c971eda92fbf73b72852
araf821 <araf.ahmed200@gmail.com>
2022-09-30 16:29:31 -0400

    restarted q2

--- a1/A1Q2.java
+++ a1/A1Q2.java
@@ -50,37 +50,10 @@ public class A1Q2 {
      */
     public static int solve(int[] arr) {
         // TODO: implement this function
-        int result = 1;
-        int i = 0;
-        A1Stack s = new A1Stack();
-//5, 4, 3, 1, 2
-        for(i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) break; // Break the loop once we've arrived at 1.
-            s.push(arr[i]); // If the element isn't a 1, then push it into the stack.
-        }
+        
 
-        // Return the total num of elements if no elements were added to the stack.
-        if (s.isEmpty()) {
-            return arr.length;
-        }
 
-        // Else...
-        int indexOf1 = i;
-        for (i += 1; i < arr.length; i++) {
-            // Loop through the array to check if the containing values in the array
-            // include the next value we're looking for, initially looking for a 2.
-            for (int j = indexOf1 + 1; j < arr.length; j++) {
-                if (arr[j] == result + 1) {
-                    result++;
-                    break;
-                }
-            }
-            if (s.pop() == result + 1) {
-                result++;
-            }
-        }
-
-        return result;
+        return 1;
     }
 
     /**
@@ -93,19 +66,19 @@ public class A1Q2 {
         int[] arr1 = {5, 4, 3, 1, 2};
         System.out.println(solve(arr1)); // Output should be 5
 
-        int[] arr2 = {4, 5, 2, 1, 3};
-        System.out.println(solve(arr2)); // Output should be 3
-
-        int[] arr3 = {1, 2, 3, 5, 4, 6, 8, 7, 9};
-        System.out.println(solve(arr3)); // Output should be 9
-
-        int[] arr4 = {4, 1, 2, 5, 3};
-        System.out.println(solve(arr4)); // Output should be 5
-
-        int[] arr5 = {7, 5, 2, 6, 3, 4, 1};
-        System.out.println(solve(arr5)); // Output should be 1
-
-        int[] arr6 = {4, 5, 1, 2, 3, 6, 7, 8};
-        System.out.println(solve(arr6)); // Output should be 8
+//        int[] arr2 = {4, 5, 2, 1, 3};
+//        System.out.println(solve(arr2)); // Output should be 3
+//
+//        int[] arr3 = {1, 2, 3, 5, 4, 6, 8, 7, 9};
+//        System.out.println(solve(arr3)); // Output should be 9
+//
+//        int[] arr4 = {4, 1, 2, 5, 3};
+//        System.out.println(solve(arr4)); // Output should be 5
+//
+//        int[] arr5 = {7, 5, 2, 6, 3, 4, 1};
+//        System.out.println(solve(arr5)); // Output should be 1
+//
+//        int[] arr6 = {4, 5, 1, 2, 3, 6, 7, 8};
+//        System.out.println(solve(arr6)); // Output should be 8
     }
 }
